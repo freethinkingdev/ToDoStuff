cf1d6abca4691aaf9416c25b670e3052448be166
Marcian Wuu <marcianwuu@gmail.com>
2017-05-20 21:39:25 +0100

    Added some simple css and jquery code to create simple draft of the list. Changed also html accordingly.

--- assets/js/todostuff.js
+++ assets/js/todostuff.js
@@ -1,3 +1,20 @@
 /**
  * Created by marcin on 20/05/17.
  */
+
+/*Class toggle on lis*/
+$("#toDoUlList li").click(function (e) {
+    $(this).toggleClass("listDone");
+    e.stopPropagation();
+});
+
+/*Creating functionality for the delete button to remove item from the list*/
+$("#toDoUlList span").on("click", function (e) {
+    e.stopPropagation();
+    console.log($(this).text());
+    $(this).parent().fadeOut(200, function () {
+        $(this).remove();
+    });
+});
+
+
