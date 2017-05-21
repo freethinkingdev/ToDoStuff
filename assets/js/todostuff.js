9abc897cd206fcca1bccea145dcb6fddf45dee0c
Marcian Wuu <marcianwuu@gmail.com>
2017-05-21 12:37:52 +0100

    Added transitions on delete functionality and input field. Added some comments. Updated css styling.

--- assets/js/todostuff.js
+++ assets/js/todostuff.js
@@ -2,7 +2,7 @@
  * Created by marcin on 20/05/17.
  */
 
-/*Class toggle on lis*/
+/* Done and dusted items class toggle on lis*/
 $("#toDoUlList").on("click","li",function (e) {
     $(this).toggleClass("listDone");
     e.stopPropagation();
@@ -17,13 +17,23 @@ $("#toDoUlList").on("click", "span", function (e) {
     });
 });
 
+/*Adding new element to the list*/
 $("#newToDoItem").on("keypress", function (e) {
+    /*Making sure enter is pressed*/
     var enterKey = e.which;
     if (enterKey === 13){
+        /*Getting typed text from the input*/
         var newItemToAddToTheList = $(this).val();
+        /*If enter is pressed, get the value of the text input and append to the end of the todo list*/
         $("#toDoUlList").append("<li class='linkLookLikeHandle'><span><i class='fa fa-trash' aria-hidden='true'></i></span> "+newItemToAddToTheList+"</li>");
+        /*Clearing text value*/
         $(this).val("");
     }
+});
+
+$("#hideInputField").css("cursor","pointer");
+$("#hideInputField").on("click",function () {
+   $("#newToDoItem").slideToggle("fast");
 });
 
 
